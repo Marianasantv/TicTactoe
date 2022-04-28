1cce19143942a44e3803e2e1e43ae41ffb4d509d
Marianasantv <103968769+Marianasantv@users.noreply.github.com>
2022-04-27 21:11:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,65 @@ def print_board(board):
 
 
 def update_board(board, position, player):
-    # TODO: modificar tablero con nuevo movimiento del jugador en posicion indicada.
-    pass
+   board[position[0]][position[1]]= player
+   return board
+    
+print(' ')
+print(' ')
 
 
 def check_for_winner(board, player):
-    # TODO: evaluar si el jugador indicado ha ganado la partida.
-    pass
+    if (board[0][0] == player and board[0][1] == player and board[0][2]== player):
+        return True
+        print(print_board)
+    elif (board[1][0] == player and board[1][1] == player and board[1][2]== player):
+        return True
+        print(print_board)
+    elif (board[2][0] == player and board[2][1] == player and board[2][2]== player):
+        return True
+        print(print_board)
+    elif (board[0][0] == player and board[1][0] == player and board[2][0]== player):
+        return True
+        print(print_board)
+    elif (board[0][1] == player and board[1][1] == player and board[2][1]== player):
+        return True
+        print(print_board)
+    elif (board[0][2] == player and board[1][2] == player and board[2][2]== player):
+        return True
+        print(print_board)
+    elif (board[0][0] == player and board[1][1] == player and board[2][2]== player):
+        return True
+        print(print_board)
+    elif (board[0][2] == player and board[1][1] == player and board[2][0]== player):
+        return True
+        print(print_board)
+    else:
+        return False
 
 
-'''
-Testing: 
-'''
+# '''
+# Testing: 
+##Testing
+
+
+
+
 board = create_empty_board()
 print_board(board)
+
+print(' ')
+print(' ')
+
+update_board(board,[1,2],"X")
+update_board(board,[1,1],"O")
+update_board(board,[2,2],"X")
+update_board(board,[0,2],"O")
+update_board(board,[2,0],"X")
+update_board(board,[2,1],"O")
+update_board(board,[0,0],"X")
+update_board(board,[0,1],"O")
+print_board(board)
+print(' ')
+print(' ')
+print(check_for_winner(board,"X"))
+print(' ')
